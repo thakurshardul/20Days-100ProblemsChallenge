b60873e08ecaf4a06601530a0ce3b54370f1558c
Shardul Vikram singh <52847968+thakurshardul@users.noreply.github.com>
2023-09-13 21:56:36 +0530

    IsAnagram.java

---
+++ DAY_03/IsAnagram.java
@@ -0,0 +1,50 @@
+Valid Anagram  (   https://leetcode.com/problems/valid-anagram/description/   )
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+====================================================================================================================================================================================================================
+
+====================================================================================================================================================================================================================
+
+  class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] arr1=new int[125];
+        int[] arr2=new int[125];
+        for(int i=0;i<s.length();i++){
+            arr1[s.charAt(i)]++;
+        }
+        for(int i=0;i<t.length();i++){
+            arr2[t.charAt(i)]++;
+        }
+        for(int i=97;i<=122;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+====================================================================================================================================================================================================================
